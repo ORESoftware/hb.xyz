ffd361ffa93ee82bb7f391c9b4159e4d466bb88d
Alex.Mills <amills@vibeirl.com>
2024-03-19 13:37:16 -0400

    squash-me

--- scripts/git/push.sh
+++ scripts/git/push.sh
@@ -5,7 +5,25 @@ set -e;
 # "cm" is the commit message; default message is "set"
 cm="${1:-'set'}"
 
+echo 'compiling to make sure it works...'
+tsc -p tsconfig.json
+echo 'done compiling with tsc'
+
+ssh-add -D
+ssh-add ~/.ssh/id_ed25519
+
+combined=""
+for arg in "${@}"; do
+  combined="${combined} ${arg}"
+done
+
+trimmed="$(echo "$combined" | xargs)"
+
+if test "${trimmed}" == '' ; then
+  trimmed="squash-me";
+fi
+
 git add .
 git add -A
-git commit -am "pdev:$cm"
+git commit -am "${trimmed}"
 git push
