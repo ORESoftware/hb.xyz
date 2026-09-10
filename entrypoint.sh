79ebeb186c0bb2b6915d8f255e9fe8d040c72cdb
Alexander Mills <alex@oresoftware.com>
2026-09-09 19:14:47 -0500

    [DEN-599] Stop entrypoint secret dumps and propagate process status

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,10 @@
 #!/usr/bin/env sh
 
-export keep_running=true
+set -eu
 
-node --version
+export vibe_with_color='no'
+export vibe_running_locally='no'
+export vibe_listen_to_stdin='no'
 
-# Define a handler function for SIGINT
-cleanup() {
-  echo "SIGINT caught, cleaning up..."
-  keep_running=false
-  # Perform any cleanup tasks here
-  exit 0
-}
-
-export cleanup;
-trap cleanup INT
-
-echo;
-echo 'env vars:';
-env | sort;
-echo 'starting application...';
-
-args="$@"
-echo "docker container arguments: '$args'"
-
-export vibe_with_color='no';
-export vibe_running_locally='no';
-export vibe_listen_to_stdin='no';
-
-
-while $keep_running; do
-
-  "${@}" || {
-    echo "Application exited with a non-zero code: '$?'";
-    sleep 3;
-  };
-
-done;
+echo 'starting application...'
+exec "$@"
